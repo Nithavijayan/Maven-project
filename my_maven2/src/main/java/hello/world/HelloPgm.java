be3577f69f93f666c41904c05a8c3b4fefadfb53
Nithavijayan <nitha.v.avrn@gmail.com>
2024-07-30 20:16:19 +0530

    maven program

---
+++ my_maven2/src/main/java/hello/world/HelloPgm.java
@@ -0,0 +1,18 @@
+package hello.world;
+
+public class HelloPgm 
+{
+	public void method()
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) 
+	{
+		HelloPgm obj=new HelloPgm();
+		obj.method();
+		// TODO Auto-generated method stub
+
+	}
+
+}
